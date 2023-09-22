f4fd7dbb406efb81e787c67f6dbb102be83849d9
Breno Medeiros De Oliveira <>
2023-09-22 03:52:39 -0300

    - Hello World

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, SafeAreaView  } from 'react-native'
+
+function App() {
+    return  <SafeAreaView>
+                <Text>Primeiro Componente</Text>
+            </SafeAreaView>
+}
+
+export default App
