9b11361038e6b08ffd13292376276f0d855f4c3d
Breno Medeiros De Oliveira <>
2023-10-03 18:36:09 -0300

    - Secao03Aula19 - Primeiro Componente 02 - demonstração de como escrever o código de um componente de outra forma funcional - e demonstração de que mesmo as classes são funções em Java script e EcmaScript6 - criação de arquivo para teste de treinos em JS chamado PlaygroundDeJsDeBreno.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import { Text, SafeAreaView  } from 'react-native'
 
-function App() {
+
+//Forma de escrever 01
+/* function App() {
     return  <SafeAreaView>
                 <Text>Primeiro Componente</Text>
             </SafeAreaView>
 }
+export default App */
 
-export default App
+//Forma de escrever 02
+export default ()=> <SafeAreaView>
+                        <Text>Primeiro Componente!!!</Text>
+                    </SafeAreaView>
+
+
---
+++ src/PlaygroundDeJsDeBreno.js
@@ -0,0 +1,8 @@
+
+console.log('teste')
+
+class Produto {}
+console.log(typeof Produto)
+
+
+// Faca testes em JS salvando esse arquivo e depois:     Control+Alt+'N'   , e o resultado é exibido na SAIDA , Graças a extensão do VC Code chamada de "Code runner"
