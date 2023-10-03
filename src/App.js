68e0db3892de5a69ff4d36ac9ddce17352d9f5c0
Breno Medeiros De Oliveira <>
2023-10-03 19:13:39 -0300

    - Secao03Aula20 - Organizando Componentes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Text, SafeAreaView  } from 'react-native'
-
+import { Text, SafeAreaView } from 'react-native'
 
 //Forma de escrever 01
 /* function App() {
@@ -11,8 +10,21 @@ import { Text, SafeAreaView  } from 'react-native'
 export default App */
 
 //Forma de escrever 02
-export default ()=> <SafeAreaView>
-                        <Text>Primeiro Componente!!!</Text>
-                    </SafeAreaView>
+// export default ()=> <SafeAreaView>
+//                         <Text>Primeiro Componente!!!</Text>
+//                     </SafeAreaView>
 
 
+import { View } from 'react-native'
+
+import Primeiro from './componentes/Primeiro'
+
+
+export default ()=> (
+    <SafeAreaView>
+        <View>
+            {/* <Text>{1+1}</Text> */}
+            <Primeiro/>
+        </View>
+    </SafeAreaView>
+)    
