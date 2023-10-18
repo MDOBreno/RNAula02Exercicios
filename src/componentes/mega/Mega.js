d9b7acd121d44cf956a19dd8511eafddf7bb1276
Breno Medeiros De Oliveira <>
2023-10-18 02:57:06 -0300

    - Secao03Aula53 - Componente de classe: Cuidado com o this

--- src/componentes/mega/Mega.js
+++ src/componentes/mega/Mega.js
@@ -4,12 +4,17 @@ import Estilo from '../estilo'
 
 export default class Mega extends Component {
 
+    
     // Forma 1de2 de Criar estado:
     // constructor(props) {
     //     super(props)
     //     this.state = {
     //         qtdeNumeros: props.qtdeNumeros + 1000
     //     }
+
+    //     //Forma [1de3 Parte1de2] de usar a onChangeText: A função bind() serve para informar ao js 
+    //     // a quem se refere o termo 'this' dentro da funcao alterarQtdeNumero():
+    //     // this.alterarQtdeNumero = this.alterarQtdeNumero.bind(this)
     // }
     
     // Forma 2de2 de Criar estado:
@@ -17,7 +22,13 @@ export default class Mega extends Component {
         qtdeNumeros: this.props.qtdeNumeros
     }
 
-    alterarQtdeNumero(qtde) {
+    //Forma [2de3 Parte1de2] de usar a onChangeText:
+    // alterarQtdeNumero(qtde) {
+    //     this.setState({qtdeNumeros: qtde})
+    // }
+
+    //Forma [3de3 Parte1de2] de usar a onChangeText:
+    alterarQtdeNumero = (qtde) => {
         this.setState({qtdeNumeros: qtde})
     }
 
@@ -30,9 +41,20 @@ export default class Mega extends Component {
                     {this.state.qtdeNumeros}
                 </Text>
                 <TextInput 
+                    keyboardType={'numeric'}
+                    style={
+                        {
+                            borderBottomWidth: 1,
+                        }
+                    }
                     placeholder='Qtde de números'
                     value={this.state.qtdeNumeros}
-                    onChangeText={this.alterarQtdeNumero(qtde)}
+
+                    //Forma [1de3 Parte2de2] e [2de3 Parte2de2] de usar a onChangeText:
+                    // onChangeText={qtde => this.alterarQtdeNumero(qtde)}
+
+                    //Forma [3de3 Parte2de2] de usar a onChangeText:
+                    onChangeText={this.alterarQtdeNumero}
                 />
             </>
         )
